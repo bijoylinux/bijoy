c1f6be867f5c5f8a42968534bbd814b2d4069ad3
rozari0 <daniel.rozario.2005@gmail.com>
2021-04-20 00:48:35 +0600

    Update File

--- main.sh
+++ main.sh
@@ -2,6 +2,8 @@ apt install ibus-m17n
 
 chmod 777 /var/lib/dpkg/info/m17n-db.list
 
-echo "/usr/share/m17n//bn-bijoyUnicode.mim" >> /var/lib/dpkg/info/m17n-db.list
+mv bn-bijoy* /usr/share/m17n/
+
+echo "/usr/share/m17n/bn-bijoyUnicode.mim" >> /var/lib/dpkg/info/m17n-db.list
 echo "/usr/share/m17n/bn-bijoyClassic.mim" >> /var/lib/dpkg/info/m17n-db.list
 
